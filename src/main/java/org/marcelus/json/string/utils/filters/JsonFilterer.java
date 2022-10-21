b57a620014bb1af5b4b355d22e8cbf2893b4c9c8
Marcelus Mantovani <51h2u3fv@anonaddy.me>
2022-10-21 19:13:10 +0100

    Implemented a class based filter.

--- src/main/java/org/marcelus/json/string/utils/filters/JsonFilterer.java
+++ src/main/java/org/marcelus/json/string/utils/filters/JsonFilterer.java
@@ -6,34 +6,100 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class JsonFilterer {
 
+    /**
+     * Instantiates Object Mapper
+     * @return
+     */
     private static ObjectMapper instantiateObjectMapper(){
         return new ObjectMapper();
     }
-    public static String filter(String jsonString, String... keys) {
-        return Optional.ofNullable(jsonString)
-                .map(js-> {
-                    try {
-                        return instantiateObjectMapper().readValue(js, new TypeReference<Map<String,Object>>(){});
-                    } catch (JsonProcessingException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .map(map-> {
-                    try {
-                        return instantiateObjectMapper().writeValueAsString(map.entrySet().stream()
-                            .filter(keyValuePair-> Arrays.asList(keys).contains(keyValuePair.getKey()))
-                            .collect(Collectors.toMap(key -> key.getKey(), value -> value.getValue()))
-                    );
-                    } catch (JsonProcessingException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                }).orElse("");
+
+    /**
+     * Converts a string in json format to a Map<String, Object> type.
+     * @param jsonString
+     * @return
+     */
+    private static Map<String, Object> convertJsonStringToMapObject(String jsonString){
+        try {
+            return instantiateObjectMapper().readValue(jsonString, new TypeReference<Map<String,Object>>(){});
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    /**
+     * Converts any object back to JSON
+     * @param object
+     * @return
+     */
+    private static String convertObjectToJSonString(Object object){
+        try {
+            return instantiateObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Filter a "bigger" map into a "smaller" map based on the keys.
+     * @param map
+     * @param keys
+     * @return
+     */
+    private static Map<String, Object> filterMapObjectBasedOnKeys(Map<String, Object> map, String... keys){
+            return filterMapObjectBasedOnKeys(map, Arrays.asList(keys));
+    }
+
+    /**
+     * Filter a "bigger" map into a "smaller" map based on the keys. (Overloaded)
+     * @param map
+     * @param keys
+     * @return
+     */
+    private static Map<String, Object> filterMapObjectBasedOnKeys(Map<String, Object> map, List<String> keys){
+        return map.entrySet().stream()
+                .filter(keyValuePair-> keys.contains(keyValuePair.getKey()))
+                .collect(Collectors.toMap(key -> key.getKey(), value -> value.getValue()));
+    }
+
+
+    /**
+     * Filters a "bigger" json string into a "smaller" json string based on the keys.
+     * @param jsonString
+     * @param keys
+     * @return
+     */
+    public static String filter(String jsonString, String... keys) {
+        return Optional.ofNullable(jsonString)
+                .map(JsonFilterer::convertJsonStringToMapObject)
+                .map(map-> filterMapObjectBasedOnKeys(map, keys))
+                .map(JsonFilterer::convertObjectToJSonString)
+                .orElse("");
+    }
+
+    /**
+     * Filters a "bigger" json string into a "smaller" json string based on a class definition.
+     * @param jsonString
+     * @param tClass
+     * @return
+     */
+    public static <T> String filter(String jsonString, Class<T> tClass) {
+        return Optional.ofNullable(jsonString)
+                .map(JsonFilterer::convertJsonStringToMapObject)
+                .map(map->filterMapObjectBasedOnKeys(map, Arrays.asList(tClass.getDeclaredFields())
+                        .stream()
+                        .map(field->field.getName())
+                        .collect(Collectors.toList())))
+                .map(JsonFilterer::convertObjectToJSonString)
+                .map(filteredJsonString-> new JsonString2ClassConverter<T>(tClass).convert(filteredJsonString))
+                .map(JsonFilterer::convertObjectToJSonString)
+                .orElse("");
+    }
 }
---
+++ src/test/java/org/marcelus/json/string/utils/testclassess/PersonWithNameAndAge.java
@@ -0,0 +1,14 @@
+package org.marcelus.json.string.utils.testclassess;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonWithNameAndAge {
+
+    private String name;
+    private int age;
+}
